97132207c81af78f7b707be933de228a491763a6
ascuns <ascuns@users.noreply.github.com>
2015-09-01 17:08:33 -0700

    First round of annotation

--- C++/TestAddin2/Connect.h
+++ C++/TestAddin2/Connect.h
@@ -6,13 +6,70 @@
 #include "TestAddin2_i.h"
 #include "FormRegionWrapper.h";
 
+// ###00000
+// ------------------------------------------
+// This file is built off the tutorial found at
+// https://msdn.microsoft.com/en-us/library/office/ee941475%28v=office.14%29.aspx
+// (retrieved: 1 sept 2015)
+//    Added annotation is marked by the ###00nnn comments.
+
 #if defined(_WIN32_WCE) && !defined(_CE_DCOM) && !defined(_CE_ALLOW_SINGLE_THREADED_OBJECTS_IN_MTA)
 #error "Single-threaded COM objects are not properly supported on Windows CE platform, such as the Windows Mobile platforms that do not include full DCOM support. Define _CE_ALLOW_SINGLE_THREADED_OBJECTS_IN_MTA to force ATL to support creating single-thread COM object's and allow use of it's single-threaded COM object implementations. The threading model in your rgs file was set to 'Free' as that is the only threading model supported in non DCOM Windows CE platforms."
 #endif
 
+// ###00001
+// >> Step 3: Implement Interfaces
+//---------------------------------
+// COM interfaces all derive from IDispatch.
+
+// The IDispatchImpl template declares the major/minor version number
+// of the interface to look up **in the registry**.
+
+// Note: if the Major and minor version numbers are 0xFFFF, the
+// IDispatchImpl class will look for the version numbers in the library
+// file.
+// see: http://stackoverflow.com/questions/20152185/using-idispatchimpl-with-an-unregistered-interface-in-an-mfcatl-exe
+
+// Problem #1: if you have multiple version installed, you get pot luck
+// which library gets found.  And if you are not writing for the least
+// common denominator, you may get unexpected behavior.
+
+// Problem #2: ... what if the library is not included in the path?
+
+// Note that a side effect of looking for it in the registry is that
+// you have to have it registered (IE installed).  That is, ... have
+// Outlook installed.
+
+// ------------------------------------------------------------------------
+
+
+// ###00002
+// >> Step 3: Implement Interfaces
+//---------------------------------
+// ... So Why a typedef in the first place?  Two reasons...
+//
+// 1) because this is a long line, subject to possible change, and it
+// makes for a simpler (appearing) class definition.
+
+// 2) because IRibbonExtensibility is not in a standard interface
+// library used by VS 2010.  The walkthrough could not direct you to
+// add it to the project in the same way it directed you to add the
+// IDTExtensibility2 object. It needed to be added by hand, so why not
+// have a copy/paste example to draw from?
+
+// Side effect: as we didn't use the Wizard to add the Ribbon Extension
+// object, we have to do the handling (like was done for
+// IDTExtensibility2) by hand.
+
+// See also: (creating a stand alone type library for the ribbon)
+// https://xldennis.wordpress.com/2006/12/12/creating-a-standalone-type-library-for-iribbonextensibility/
+
 
 typedef IDispatchImpl<_IDTExtensibility2, &__uuidof(_IDTExtensibility2), &__uuidof(__AddInDesignerObjects), /* wMajor = */ 1>
-	IDTImpl;
+    IDTImpl;
+// See also: (IDTExtensibility2 interface)
+// https://msdn.microsoft.com/en-us/library/extensibility.idtextensibility2.aspx
+
 typedef IDispatchImpl<IRibbonCallback, &__uuidof(IRibbonCallback)> 
 	CallbackImpl;
 typedef IDispatchImpl<_FormRegionStartup, &__uuidof(_FormRegionStartup), &__uuidof(__Outlook), /* wMajor = */ 9, /* wMinor = */ 4>
@@ -78,8 +135,28 @@ public:
 
 	// _IDTExtensibility2 Methods
 public:
-	STDMETHOD(OnConnection)(LPDISPATCH Application, ext_ConnectMode ConnectMode, LPDISPATCH AddInInst, SAFEARRAY * * custom)
-	{
+        // ###00003
+        // >> Step 3: Implement Interfaces
+        //---------------------------------
+        // These functions are all used by Outlook, called at
+        // appropriate times.  Bad things happen if we blow off these
+        // functions (by leaving the stubs returning E_NOTIMPL).  As we
+        // don't have particular needs on these events, we don't need
+        // to do more than let them return "success".
+
+        STDMETHOD(OnConnection)(LPDISPATCH Application,
+                                ext_ConnectMode ConnectMode,
+                                LPDISPATCH AddInInst,
+                                SAFEARRAY * * custom
+                               )
+        {
+            // ###00004
+            // >> Step 4: Set up loading add-in in Outlook
+            // -------------------------------------------
+            // for debugging purposes, the tutorial temporarily adds a
+            // MessageBox call here, to show that the add-in has
+            // actually been loaded.  It is taken out soon after, once
+            // you've verified that your add-in is correctly loading.
 		return S_OK;
 	}
 	STDMETHOD(OnDisconnection)(ext_DisconnectMode RemoveMode, SAFEARRAY * * custom)
--- C++/TestAddin2/stdafx.h
+++ C++/TestAddin2/stdafx.h
@@ -25,6 +25,25 @@
 #include <atlstr.h>
 #include <atlsafe.h>
 //#import "C:\Program Files\Common Files\DESIGNER\MSADDNDR.DLL" raw_interfaces_only, raw_native_types, no_namespace, named_guids, auto_search ,auto_rename
+
+// ###00006
+// >> Adding a Ribbon and Ribbon Button
+//----------------------------------------------
+
+// See: http://blogs.msmvps.com/carlosq/2013/04/06/msaddndr-dll-file-not-installed-by-office-2013/
+
+// This demo was created and last modified in 2011.  Since then,
+// Microsoft has stopped including MSADDNDR.DLL with Office.  Bitrot.
+// To make this demo work, MSADDNDR.DLL would need to be included by
+// the addin installer.
+
+// the fact that the direct import (above) is replaced by calling the
+// library ID in the registry ( below ) is for the moment mystifying.
+
+// See also: KB 2792179 on including the library manually.
+// https://support.microsoft.com/en-us/kb/2792179
+//-----------------------------------------------------------------------
+
 //#import "C:\Program Files\Microsoft Office\Office14\MSOUTL.OLB" raw_interfaces_only, raw_native_types,  named_guids, auto_search, auto_rename,rename_namespace("Outlook")
 //#import "C:\Program Files\Common Files\Microsoft Shared\OFFICE14\MSO.DLL" raw_interfaces_only, named_guids, auto_search, auto_rename,rename_namespace("Office")
 
@@ -50,6 +69,34 @@ using namespace AddinDesign;
 ////version("1.0") lcid("0")  raw_interfaces_only named_guids
 //
 //using namespace Word;
+
+// ###00007
+// >> Adding a Ribbon and Ribbon Button
+//----------------------------------------------
+// Note that the Ribbon extensibility interface may be
+// "000C0396-0000-0000-C000-000000000046"
+// See:
+// https://msdn.microsoft.com/en-us/library/microsoft.office.core.iribbonextensibility.aspx?cs-save-lang=1&cs-lang=vb#code-snippet-1
+
+// ###00009
+// >> Adding a Ribbon and Ribbon Button
+//----------------------------------------------
+// The demo gets around the ribbon issue by importing the entire Office
+// library: mso.dll
+// The key to that is libid:2DF8D04C-5BFA-101B-BDE5-00AA0044DE52   
+// just as used above.
+
+//----------------------
+// To find libid values:
+//----------------------
+// oleview.exe will provide you typelib (= libid) values.
+//   You will, though, have to search for the library you are
+//   importing.  oleview.exe is included in the Windows SDK, which you
+//   may need to install.
+//
+// See: http://www.codeproject.com/Articles/3699/Importing-Type-Libraries
+
+//---------------------------------------------------------------------------
 
 
 using namespace ATL;
